eb9ac059993b07ea13a491363cc8ad684491a017
aclaudtech-lgtm <aclaud.technologies@gmail.com>
2025-10-27 22:52:48 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional: Smooth scrolling for older browsers
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
